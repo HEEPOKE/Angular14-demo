689771333c4f45ab353753147e7e6558fcbd0198
HEEPOKE <Damon1fx@gmail.com>
2023-04-27 14:59:48 +0700

    :money_with_wings: refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { UsersComponent } from './pages/users/users.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: HomeComponent,
+  },
   { path: 'manage-users', component: UsersComponent },
 ];
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  template: `<p>home works!</p>`,
 })
 export class HomeComponent {
 
